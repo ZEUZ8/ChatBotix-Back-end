d46b7885c016d7d34f8c719c7a6ef28141721eab
sinan <sinan@sinans-MacBook-Air.local>
2023-09-15 16:24:35 +0530

    added the gitignore

--- index.js
+++ index.js
@@ -9,19 +9,32 @@ const app = express()
 
 const port = process.env.PORT || 5000
 
-app.post('/',async(req,res)=>{
+async function main(){
   const openai = new OpenAI({
     apiKey:process.env.OPENAI_API_KEY
   })
 
   const chatCompletion = await openai.chat.completions.create({
     model:"gpt-3.5-turbo",
-    messages:[{"role":"user","content":"how are you"}],
+    messages:[{"role":"user","content":"hello"}],
     max_tokens:40
   })
   console.log(chatCompletion.choices[0].message,' the message we got')
-  res.json({data:chatCompletion.data})
-})
+}
+main()
+// app.post('/',async(req,res)=>{
+//   const openai = new OpenAI({
+//     apiKey:process.env.OPENAI_API_KEY
+//   })
+
+//   const chatCompletion = await openai.chat.completions.create({
+//     model:"gpt-3.5-turbo",
+//     messages:[{"role":"user","content":"how are you"}],
+//     max_tokens:40
+//   })
+//   console.log(chatCompletion.choices[0].message,' the message we got')
+//   res.json({data:chatCompletion.data})
+// })
 
 app.listen(port,()=>{
   console.log('Project is running on',port)
