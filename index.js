840ced9ca7921c91fe2df59fa5414d53412c909e
sinan <sinan@sinans-MacBook-Air.local>
2023-09-16 14:13:03 +0530

    Backend development for AI chatbot, including error handling, is complete.

--- index.js
+++ index.js
@@ -1,41 +1,34 @@
-// "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"
 const express = require("express");
 require("dotenv").config();
-
+const cors = require("cors")
+const bodyParser = require("body-parser")
 const OpenAI = require("openai")
-// main()
+const openai = new OpenAI({
+  apiKey:process.env.OPENAI_API_KEY
+})
 
 const app = express()
 
+app.use(bodyParser.json())
+app.use(cors())
+
 const port = process.env.PORT || 5000
 
-async function main(){
-  const openai = new OpenAI({
-    apiKey:process.env.OPENAI_API_KEY
-  })
+app.post('/',async(req,res)=>{
+  const {message} = req.body;
+  try{
+     const chatCompletion = await openai.chat.completions.create({
+       model:"gpt-3.5-turbo",
+       messages:[{"role":"user","content":`${message}`}],
+       max_tokens:100,
+       temperature:0.5
+     })
+     res.status(200).json({data:chatCompletion?.choices[0]?.message})
+  }catch(error){
+    res.status(500).json({err:error?.message})
+  }
 
-  const chatCompletion = await openai.chat.completions.create({
-    model:"gpt-3.5-turbo",
-    messages:[{"role":"user","content":"hello"}],
-    max_tokens:40
-  })
-  console.log(chatCompletion.choices[0].message,' the message we got')
-}
-main()
-// app.post('/',async(req,res)=>{
-//   const openai = new OpenAI({
-//     apiKey:process.env.OPENAI_API_KEY
-//   })
-
-//   const chatCompletion = await openai.chat.completions.create({
-//     model:"gpt-3.5-turbo",
-//     messages:[{"role":"user","content":"how are you"}],
-//     max_tokens:40
-//   })
-//   console.log(chatCompletion.choices[0].message,' the message we got')
-//   res.json({data:chatCompletion.data})
-// })
-
+})
 app.listen(port,()=>{
   console.log('Project is running on',port)
 })
